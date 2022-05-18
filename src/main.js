bc445816bd54f968a1bd39f608538393f5ea7a2a
Cong Nguyen <kaom.n.92@gmail.com>
2022-05-18 10:49:44 +0300

    push

--- src/main.js
+++ src/main.js
@@ -1,34 +1,16 @@
-function setFormMessage(formElement, type, message) {
-	const messageElement = formElement.querySelector(".form_message");
-
-	messageElement.textContent = message;
-	messageElement.classList.remove ("form_message-success", "form_message-error");
-	messageElement.classList.add(`form_message-${type}`);
-}
-
-function setInputError(inputElement, message) {
-	inputElement.classList.add("form_input-error");
-	inputElement.parentElement.querySelector(".input_error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-	inputElement.classList.remove("form_input-error");
-	inputElement.parentElement.querySelector(".input_error-message").textContent = "";
-}
-
 document.addEventListener("DOMContentLoaded", () => {
 	const loginForm = document.querySelector("#login");
 	const createAccountForm = document.querySelector("#createAccount");
 
 	document.querySelector("#linkCreateAccount").addEventListener("click", e => {
 		e.preventDefault();
-		loginForm.classList.add("form-hidden");
-		createAccountForm.classList.remove("form-hidden");
+		loginForm.classList.add("form_hidden");
+		createAccountForm.classList.remove("form_hidden");
 	});
 
 	document.querySelector("#linkLogin").addEventListener("click", e => {
 		e.preventDefault();
-		loginForm.classList.remove("form-hidden");
-		createAccountForm.classList.add("form-hidden");
+		loginForm.classList.remove("form_hidden");
+		createAccountForm.classList.add("form_hidden");
 	});
 });
